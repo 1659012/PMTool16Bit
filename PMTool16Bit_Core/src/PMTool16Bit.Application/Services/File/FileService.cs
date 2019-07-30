2678b1c950833a99b12078ae49206d5500994103
Hoan <lenguyenhanhoan@gmail.com>
2019-07-30 18:55:27 +0700

    update upload file in EventTask detail

--- PMTool16Bit_Core/src/PMTool16Bit.Application/Services/File/FileService.cs
+++ PMTool16Bit_Core/src/PMTool16Bit.Application/Services/File/FileService.cs
@@ -1,7 +1,10 @@
 ﻿using Abp.Application.Services;
 using Abp.Domain.Repositories;
+using Abp.Extensions;
+using Microsoft.EntityFrameworkCore;
 using PMTool16Bit.Models;
 using PMTool16Bit.Models.Enum;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -33,6 +36,35 @@ namespace PMTool16Bit.Services
             var file = GetFileById(fileId).Result;
             var entityPath = Path.Combine(baseUrl, file.Path);
             return entityPath;
+        }
+
+        public async Task<List<FileEntityDto>> GetAllFileFromIds(string idString)
+        {
+            if (idString.IsNullOrEmpty())
+            {
+                return new List<FileEntityDto>();
+            }
+
+            var ids = idString.Split(',');
+
+            var pictureList = await Repository.GetAll()
+                .Where(p => ids.Contains(p.Id.ToString()))
+                .Select(p => new FileEntityDto
+                {
+                    Name = p.Name,
+                    Id = p.Id,
+                    Path = p.Path
+                })
+                .ToListAsync();
+
+            var baseUrl = await SettingManager.GetSettingValueAsync(SettingKey.App_BaseUrl);
+
+            pictureList.ForEach(p =>
+            {
+                p.Path = Path.Combine(baseUrl, p.Path);
+            });
+
+            return pictureList;
         }
     }
 }
