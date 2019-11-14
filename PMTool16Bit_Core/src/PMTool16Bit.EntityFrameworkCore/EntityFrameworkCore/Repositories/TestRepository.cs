44cec5873456aed550d757e4049a4a7c1c1e3773
Hoan Le <lenguyenhanhoan@gmail.com>
2019-11-14 16:31:27 +0700

    add azure connection

---
+++ PMTool16Bit_Core/src/PMTool16Bit.EntityFrameworkCore/EntityFrameworkCore/Repositories/TestRepository.cs
@@ -0,0 +1,38 @@
+﻿using Abp.Data;
+using Abp.EntityFrameworkCore;
+using Abp.ObjectMapping;
+using Microsoft.EntityFrameworkCore;
+using NPOI.SS.Formula.Functions;
+using PMTool16Bit.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMTool16Bit.EntityFrameworkCore.Repositories
+{
+    public class TestRepository: PMTool16BitRepositoryBase<EventTask>
+    {
+        private readonly IActiveTransactionProvider transactionProvider;
+        private readonly IObjectMapper objectMapper;
+
+        public TestRepository(IDbContextProvider<PMTool16BitDbContext> dbContextProvider,
+            IActiveTransactionProvider transactionProvider,
+            IObjectMapper objectMapper)
+            : base(dbContextProvider)
+        {
+            this.transactionProvider = transactionProvider;
+            this.objectMapper = objectMapper;
+        }
+
+        //public string GetExecuteStoredProc()
+        //{
+        //    var commandText = "Select * from EventTasks";           
+        //    var events = Context.Database.ExecuteSqlCommand(commandText);
+        //    return events;
+        //}
+    }
+}
