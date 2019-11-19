8def4427e40d88abd9e4fa6eb01cf7b5bedd5567
Hoan <lenguyenhanhoan@gmail.com>
2019-11-19 23:16:57 +0700

    update TaskCalendar

--- PMTool16Bit_SPA/src/views/dashboard/TaskCalendar.vue
+++ PMTool16Bit_SPA/src/views/dashboard/TaskCalendar.vue
@@ -1,18 +1,24 @@
 <template>
   <div style="height:100%">
-    <v-calendar ref="calendar" v-model="startDate" :type="type" :end="endDate" color="primary">
+    <v-calendar ref="calendar" v-model="start" :type="type" color="primary">
       <template v-slot:day="{ date }">
-        <div v-if="eventsMap[date]&&eventsMap[date].length<=3">
+        <div v-if="eventsMap[date] && eventsMap[date].length <= 3">
           <template v-for="event in eventsMap[date]">
             <TaskPopover :key="event.title" :event="event" />
           </template>
         </div>
 
-        <div v-else-if="eventsMap[date] && eventsMap[date].length>3">
+        <div v-else-if="eventsMap[date] && eventsMap[date].length > 3">
           <v-menu offset-x :close-on-content-click="false">
             <template v-slot:activator="{ on }">
-              <v-btn color="primary" flat dark v-on="on" class="text-capitalize ml-0">
-                {{eventsMap[date].length}} tasks
+              <v-btn
+                color="primary"
+                flat
+                dark
+                v-on="on"
+                class="text-capitalize ml-0"
+              >
+                {{ eventsMap[date].length }} tasks
                 <v-icon>arrow_right</v-icon>
               </v-btn>
             </template>
@@ -27,15 +33,15 @@
     </v-calendar>
     <v-layout row wrap>
       <v-flex lg4 class="text-sm-left text-xs-center">
-        <v-btn @click="$refs.calendar.prev()">
+        <v-btn @click="$refs.calendar.prev();setPreviousMonth()">
           <v-icon dark left>keyboard_arrow_left</v-icon>Prev
         </v-btn>
       </v-flex>
-      <v-flex lg4 class="text-xs-center">
-        <!-- <v-select v-model="type" :items="typeOptions" label="Type"></v-select> -->
-      </v-flex>
+      <v-flex lg4 class="text-xs-center"
+        ><p class="font-weight-medium mt-3">{{ startDate | date }}</p></v-flex
+      >
       <v-flex lg4 class="text-sm-right text-xs-center">
-        <v-btn @click="$refs.calendar.next()">
+        <v-btn @click="$refs.calendar.next();setNextMonth()">
           Next
           <v-icon right dark>keyboard_arrow_right</v-icon>
         </v-btn>
@@ -45,22 +51,15 @@
 </template>
 
 <script>
+import moment from "moment";
 import TaskPopover from "./TaskPopover";
 export default {
   components: { TaskPopover },
   data: () => ({
     events: [],
     type: "month",
-    startDate: "2019-07-01",
-    endDate: "2019-07-31",
-    typeOptions: [
-      { text: "Day", value: "day" },
-      { text: "4 Day", value: "4day" },
-      { text: "Week", value: "week" },
-      { text: "Month", value: "month" },
-      { text: "Custom Daily", value: "custom-daily" },
-      { text: "Custom Weekly", value: "custom-weekly" }
-    ]
+    startDate: "",
+    start: ""
   }),
   computed: {
     getToday() {
@@ -71,7 +70,6 @@ export default {
       this.events.forEach(e => {
         e.startDate = e.startDate.slice(0, 10);
         e.dueDate = e.dueDate.slice(0, 10);
-        // e.open = false;
         (map[e.dueDate] = map[e.dueDate] || []).push(e);
       });
       return map;
@@ -80,7 +78,9 @@ export default {
   watch: {},
   mounted() {
     this.loadEventTaskCalendar();
-    this.startDate = this.getToday;
+    this.startDate = moment()
+      .startOf("month")
+      .format("YYYY-MM-DD");
   },
   methods: {
     loadEventTaskCalendar() {
@@ -95,12 +95,22 @@ export default {
         .catch(e => {
           this.errors.push(e);
         });
+    },
+    setPreviousMonth() {
+      this.startDate = moment(this.startDate)
+        // .startOf('month') //if start is not first date of month
+        .subtract(1, "days")
+        .startOf("month")
+        .format("YYYY-MM-DD");
+    },
+    setNextMonth() {
+      this.startDate = moment(this.startDate)
+        .endOf("month")
+        .add(1, "days")
+        // .startOf('month') //if start is not last date of month
+        .format("YYYY-MM-DD");
     }
   }
 };
 </script>
-<style  scoped>
-</style>
-
-
-
+<style scoped></style>
